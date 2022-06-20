f17944d22e348921f41936355865aa939db13e2f
Telesphoreo <me@telesphoreo.me>
2022-06-19 22:30:20 -0500

    CSS changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,16 @@ function App() {
     return (
         <BrowserRouter>
             <Navbar bg={"dark"} variant={"dark"}>
-                <Navbar.Brand className={"ms-5"} href={"/"}>SCISSORS</Navbar.Brand>
+                <Navbar.Brand className={"ms-5"} href={"/"}>Scissors</Navbar.Brand>
                 <Nav className={"me-auto"}>
                     <NavLink href={"https://github.com/AtlasMediaGroup/Scissors"}>Source</NavLink>
                     <NavLink href={"https://status.scissors.gg"}>Status</NavLink>
                 </Nav>
-                <Button variant={"light"} className={"me-5 download-button"} href={"/downloads"} style={{borderRadius: "2vh"}}><img src={"https://cdn3.iconfinder.com/data/icons/download-buttons-outline-collection/70/Download-13-512.png"} width={"50px"} alt={"download"}/>  DOWNLOAD</Button>
+                <Button variant={"primary"} className={"me-5 download-button"} href={"/downloads"} style={{ borderRadius: "2vh" }}><img src={"download-icon.png"} width={"25px"} alt={"download"} />  Download</Button>
             </Navbar>
             <Routes>
-                <Route path={"/"} element={<HomePage/>}/>
-                <Route path={"/downloads"} element={<DownloadPage/>}/>
+                <Route path={"/"} element={<HomePage />} />
+                <Route path={"/downloads"} element={<DownloadPage />} />
             </Routes>
         </BrowserRouter>
     );
